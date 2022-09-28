66e404112b5aa711318489bf31f7802ab6560720
pamelafett <108582417+pamelafett@users.noreply.github.com>
2022-09-28 16:22:44 +0100

    Add files via upload

---
+++ exercicioCriacaoJAVA.java
@@ -0,0 +1,29 @@
+package criacaoJAVA;
+
+public class exercicioCriacaoJAVA {
+
+	public static void main(String[] args) {
+		
+		int a = 2;
+		int b = 3;
+		int c = 4;
+		int d = 6;
+		
+		System.out.println(a + b);
+		System.out.println(b - a);
+		System.out.println(a * c);
+		System.out.println(d / a);
+
+		System.out.println("Boa tarde");
+		System.out.println("Agora são 15h30");
+		
+		System.out.println("Valor Solicitado Empréstimo 3000€");
+		
+		int s1 = 3000;
+		int g1 = 1400;
+		
+		System.out.println(s1 - g1);
+		
+	}
+
+}
